4c05a0cc2b4f76cffb69a4735fc58e3b314d6502
rnfisher <rnf.fisher@knights.ucf.edu>
2019-09-18 20:14:26 -0400

    This is Challenge 1, a modified version of Tutorial 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector2(100.0f, 0.0f);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "" +
+            "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "You win! Game created by Ryan Fisher!";
+        }
+    }
+}
