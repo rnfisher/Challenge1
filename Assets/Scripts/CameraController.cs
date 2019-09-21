9c5e6881f37fa695e764655c23cfebe110750812
rnfisher <rnf.fisher@knights.ucf.edu>
2019-09-21 14:18:31 -0400

    Challenge1 is a modified version of Tutorial 1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,11 +13,16 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         offset = transform.position - player.transform.position;
+
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,29 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public Text countText;
+    public Text livesText;
     public Text winText;
+    public Text winTextShadow;
+    public Text levelText;
+    public Text levelTextShadow;
+
 
     private Rigidbody2D rb2d;
     private int count;
+    private int lives;
 
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
+        lives = 3;
         winText.text = "";
+        winTextShadow.text = "";
+        levelText.text = "Level 1";
+        levelTextShadow.text = "Level 1";
         SetCountText();
-
+        SetLivesText();
     }
 
     void FixedUpdate()
@@ -28,9 +38,6 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce (movement * speed);
-
-        if (Input.GetKey("escape"))
-            Application.Quit();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -41,15 +48,33 @@ public class PlayerController : MonoBehaviour {
             count = count + 1;
             SetCountText();
         }
-        else if (other.gameObject.CompareTag("Enemy"))
+
+        if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
-            count = count - 1;
-            SetCountText();
+            lives = lives - 1;
+            SetLivesText();
         }
+
         if (count == 12)
         {
             transform.position = new Vector2(100.0f, 0.0f);
+            rb2d.velocity = new Vector2(0.0f, 0.0f);
+            levelText.text = "Level 2";
+            levelTextShadow.text = "Level 2";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "" +
+           "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "You Lose!";
+            winTextShadow.text = "You Lose!";
+            rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            Destroy(this);
         }
     }
 
@@ -60,6 +85,13 @@ public class PlayerController : MonoBehaviour {
         if (count >= 20)
         {
             winText.text = "You win! Game created by Ryan Fisher!";
+            winTextShadow.text = "You win! Game created by Ryan Fisher!";
+               if (lives == 3)
+            {
+                winText.text = "Perfect! You win! Game created by Ryan Fisher!";
+                winTextShadow.text = "Perfect! You win! Game created by Ryan Fisher!";
+            }
+            rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 }
